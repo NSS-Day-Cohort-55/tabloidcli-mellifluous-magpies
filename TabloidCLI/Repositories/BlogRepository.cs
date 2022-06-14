a328991639c0f421355751c1c07d2288f89527b0
e-bierschenk <bushanks@gmail.com>
2022-06-14 13:22:39 -0500

    get all blogs

---
+++ TabloidCLI/Repositories/BlogRepository.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TabloidCLI.Repositories;
+using TabloidCLI.Models;
+using Microsoft.Data.SqlClient;
+
+namespace TabloidCLI
+{
+    public class BlogRepository : DatabaseConnector, IRepository<Blog>
+    {
+        public BlogRepository(string connectionString) : base(connectionString) { }
+
+        public void Insert(Blog blog)
+        {
+
+        }
+
+        public List<Blog> GetAll()
+        {
+            using (SqlConnection conn = Connection)
+            {
+                conn.Open();
+                using (SqlCommand cmd = conn.CreateCommand())
+                {
+                    cmd.CommandText = "SELECT Id, Title, Url FROM Blog;";
+
+                    using (SqlDataReader reader = cmd.ExecuteReader())
+                    {
+                        List<Blog> blogs = new List<Blog>();
+                        while (reader.Read())
+                        {
+                            Blog blog = new Blog
+                            {
+                                Id = reader.GetInt32(reader.GetOrdinal("Id")),
+                                Title = reader.GetString(reader.GetOrdinal("Title")),
+                                Url = reader.GetString(reader.GetOrdinal("Url"))
+                            };
+                            blogs.Add(blog);
+                        }
+                        return blogs;
+                    }
+                }
+            }
+        }
+
+        public void Update(Blog blog)
+        {
+
+        }
+
+        public void Delete(int i)
+        {
+
+        }
+
+        public Blog Get(int i)
+        {
+            return null;
+        }
+
+
+
+
+    }
+}
