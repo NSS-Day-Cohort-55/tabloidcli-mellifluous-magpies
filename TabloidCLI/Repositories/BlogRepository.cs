2a47da5cf45167d3f81af7dbaa852040dc60eaeb
k-sarito <coryclark95@gmail.com>
2022-06-14 11:47:48 -0500

    jk didn't save

--- TabloidCLI/Repositories/BlogRepository.cs
+++ TabloidCLI/Repositories/BlogRepository.cs
@@ -3,10 +3,78 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using TabloidCLI.Repositories;
+using TabloidCLI.Models;
+using Microsoft.Data.SqlClient;
 
-namespace TabloidCLI.Repositories
+namespace TabloidCLI
 {
-    internal class BlogRepository
+    public class BlogRepository : DatabaseConnector, IRepository<Blog>
     {
+        public BlogRepository(string connectionString) : base(connectionString) { }
+
+        public void Insert(Blog blog)
+        {
+
+        }
+
+        public List<Blog> GetAll()
+        {
+            return null;
+
+        }
+
+        public void Update(Blog blog)
+        {
+            using (SqlConnection conn = Connection)
+            {
+                conn.Open();
+                using (SqlCommand cmd = conn.CreateCommand())
+                {
+                    cmd.CommandText = @"UPDATE Blog
+                                        SET Title = @title,
+                                        URL = @url
+                                        WHERE Id = @id";
+                    cmd.Parameters.AddWithValue("@title", blog.Title);
+                    cmd.Parameters.AddWithValue("@url", blog.Url);
+                    cmd.Parameters.AddWithValue("@id", blog.Id);
+
+                    cmd.ExecuteNonQuery();
+                }
+            }
+        }
+
+        public void Delete(int i)
+        {
+
+        }
+
+        public Blog Get(int i)
+        {
+            using (SqlConnection conn = Connection)
+            {
+                conn.Open();
+                using (SqlCommand cmd = conn.CreateCommand())
+                {
+                    cmd.CommandText = "SELECT Title, URL FROM Blog WHERE Id = @id";
+                    cmd.Parameters.AddWithValue("@id", i);
+
+                    using (SqlDataReader reader = cmd.ExecuteReader())
+                    {
+                        Blog blog = null;
+                        if (reader.Read())
+                        {
+                            blog = new Blog
+                            {
+                                Id = i,
+                                Title = reader.GetString(reader.GetOrdinal("Title")),
+                                Url = reader.GetString(reader.GetOrdinal("URL"))
+                            };
+                        }
+                        return blog;
+                    }
+                }
+            }
+        }
     }
 }
